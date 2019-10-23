2daa29e2ffe87cc31070134c8c0ee9da5d46b394
Ryan Milner <ryantmilner@hotmail.co.uk>
2019-10-23 11:45:13 +0100

    Added distributed wfuzz

--- README.md
+++ README.md
@@ -5,5 +5,5 @@
 * [Challenge Dashboard](https://www.cybercrime.co.uk/dashboard-challenges)
 
 ## Useful commands
-* wfuzz form for tty: `wfuzz --oF wfuzz-log -u http://3.cybertrial.co.uk/login -b "PHPSESSID=rk1meepo069lvoo34udt5agqgn" -d "formgo=1&username=FUZZ&password=test" -t 1 -s 0.7 -w [WORDLIST] --hs [IGNORE_STRING] -c`
+* wfuzz form for tty: `wfuzz --oF wfuzz-log -u http://3.cybertrial.co.uk/login -b "PHPSESSID=rk1meepo069lvoo34udt5agqgn" -d "formgo=1&username=FUZZ&password=test" -s 0.7 -w [WORDLIST] --hs [IGNORE_STRING] -c`
 * run in background: `nohup [COMMAND_STRING] &`
---
+++ distributed-wfuzz/client.py
@@ -0,0 +1,67 @@
+import wfuzz
+import json
+import socket
+import pickle
+
+input_string = "-u http://testphp.vulnweb.com/listproducts.php?cat=FUZZ -w ./test_wl.txt --hl 97 -s 0.7"
+
+
+nodes = ["127.0.0.1:65432", "127.0.0.1:65433"]
+
+def chunker_list(seq, size):
+    return (seq[i::size] for i in range(size))
+
+s = wfuzz.get_session(input_string)
+wl = json.loads(s.export_json())["wfuzz_recipe"]["payloads"][0][1]["default"]
+
+print("Reading wordlist: " + wl + "...")
+with open(wl, 'r') as f:
+    lines = f.read().splitlines()
+
+print("Splitting up...")
+wl_parts = list(chunker_list(lines, len(nodes)))
+
+print("Sending to clients...")
+nodeSockets = {}
+nodeNum = 0
+for node in nodes:
+    try:
+        nodeInfo = node.split(":")
+        nodeSockets[node] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        nodeSockets[node].connect((nodeInfo[0], int(nodeInfo[1])))
+        nodeSockets[node].sendall((input_string + "\n").encode("utf-8"))
+        nodeSockets[node].sendall(pickle.dumps(wl_parts[nodeNum]))
+        
+        nodeNum = nodeNum + 1
+    except:
+        print("Error connecting to " + node)
+        pass
+
+if (nodeNum < 1):
+    print("Error connecting to all nodes, exiting!")
+    exit()
+
+print("Waiting for responses")
+while len(nodeSockets) >= 1:
+    for nodeIP in nodeSockets:
+        conn = nodeSockets[nodeIP]
+
+        curLine = ""
+        while True:
+            data = conn.recv(1)
+            if not data:
+                break
+            
+            if (data == b'\n'):
+                if (curLine == "$$"):
+                    del(nodeSockets[nodeIP])
+                    break
+                print(curLine)
+                curLine = ""
+                break
+            else:
+                curLine += data.decode("utf-8")
+
+
+
+print("Done")
