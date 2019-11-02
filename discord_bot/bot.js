d9de7618cac62e3f5da1fe74bed92a5d94459162
rtm516 <ryantmilner@hotmail.co.uk>
2019-11-02 12:51:46 +0000

    Updated bot to add reconnect and better logging

--- discord_bot/bot.js
+++ discord_bot/bot.js
@@ -1,13 +1,17 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
 const ytdl = require("ytdl-core");
-const opus = require('opusscript');
+const opus = require("opusscript");
 const request = require("request");
-const cheerio = require('cheerio');
-const fs = require('fs');
+const cheerio = require("cheerio");
+const fs = require("fs");
 
-client.on('ready', () => {
+require('./override-console-log.js');
+
+const token = "NjM5OTY5MDAxOTU0NjA3MTM0.Xby_og.xCjT9tCeYgbc2fGcjUkl5QCdz_w";
+
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 
   checkChallengeCount();
@@ -16,11 +20,18 @@ client.on('ready', () => {
   }, 60 * 1000);
 });
 
+client.on("disconnect", function(event){
+  console.log(`The WebSocket has closed, manually reconnecting!`);
+
+  client.login(token);
+});
+
 function checkChallengeCount() {
+  console.log("Checking challenge count");
 
   var options = {
-    method: 'GET',
-    url: 'https://www.cybercrime.co.uk/dashboard-challenges',
+    method: "GET",
+    url: "https://www.cybercrime.co.uk/dashboard-challenges",
     headers: {
       "Cookie": "PHPSESSID=CHANGE_ME"
     }
@@ -36,6 +47,8 @@ function checkChallengeCount() {
       encoding: "utf-8"
     }));
 
+    console.log("Challenge count: " + chal_count + " vs " + chal_count_last);
+
     if (chal_count > chal_count_last) {
       fs.writeFile("chal_count.txt", chal_count, () => {})
       panic(chal_count);
@@ -44,6 +57,8 @@ function checkChallengeCount() {
 }
 
 function panic(chal_count) {
+  console.log("New challenge alerting!");
+
   client.channels.get("517388963665805334").send("New challenge! https://www.cybercrime.co.uk/dashboard-challenge-preview/10" + chal_count + " <@170429173003714560> <@505414674003656765> <@220548182650781696> <@215715196960768002> <@545297878172827659>");
 
   const streamOptions = {
@@ -52,17 +67,17 @@ function panic(chal_count) {
   };
 
   client.channels.get("517389057102184462").join().then(connection => {
-    console.log("joined channel");
-    const stream = ytdl('https://www.youtube.com/watch?v=dQw4w9WgXcQ', {
-      filter: 'audioonly'
+    console.log("Joined voice channel");
+    const stream = ytdl("https://www.youtube.com/watch?v=dQw4w9WgXcQ", {
+      filter: "audioonly"
     });
     const dispatcher = connection.playStream(stream, streamOptions);
     dispatcher.on("end", end => {
-      console.log("left channel");
+      console.log("Left voice channel");
       voiceChannel.leave();
     });
   }).catch(err => console.log(err));
 }
 
 //https://discordapp.com/oauth2/authorize?&client_id=639969001954607134&scope=bot&permissions=0
-client.login('NjM5OTY5MDAxOTU0NjA3MTM0.Xby_og.xCjT9tCeYgbc2fGcjUkl5QCdz_w');
+client.login(token);
