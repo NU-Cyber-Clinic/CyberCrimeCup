0fdafd21096826ecf8d3e81c47881db6208a0a4f
rtm516 <ryantmilner@hotmail.co.uk>
2019-11-06 21:33:50 +0000

    Challenge 110

---
+++ 2019/110 The Steg Bonus/solve.py
@@ -0,0 +1,17 @@
+from PIL import Image
+import binascii
+
+im = Image.open('see-you-soon.png')
+
+string = ""
+
+width, height = im.size
+for y in range(height):
+	for x in range(width):
+		r,g,b = im.getpixel((x,y))
+		if (r == 255):
+			string += "1"
+		if (r == 0):
+			string += "0"
+
+print(string)
